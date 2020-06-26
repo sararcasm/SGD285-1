b62cecc3cab9f837b8e6b163e7de5b43ea9131e2
trrdt <trrd52ae@email.cpcc.edu>
2020-06-26 16:37:28 -0400

    Fixes a bug that caused the program not to compile.

--- Main.cs
+++ Main.cs
@@ -61,8 +61,8 @@ public class SGD285_1
            new UserQuote("Aliyah Day", "We delight in the beauty of the butterfly, but rarely admit the changes it has gone through to achieve that beauty. -Maya Angelou"),
            new UserQuote("Perry Courter", "Hell of a thing killing a man, you take away all he has, and everything he is ever gunna be - Clint Eastwood" ), /* A clasic quote from a clasic man - Adam Seymour */
            new UserQuote("Thomas Robertson", "I come to trade and to learn. - Barnabas Sackett"),
-           new UserQuote("This is a Test", "Something witty is supposed to go here = Me")
-	   new UderQuote("Adam Seymour", "Lift itself is only a vision, a dream. nothing exists except empty space, and you. And you are but a thought - Mark Twain Mysterious stranger"),
+           new UserQuote("This is a Test", "Something witty is supposed to go here = Me"),
+	   new UserQuote("Adam Seymour", "Lift itself is only a vision, a dream. nothing exists except empty space, and you. And you are but a thought - Mark Twain Mysterious stranger")
            // add the next user here using the formula above, don't forget the comma to separate the items in the initialization list.
            // IMPORTANT: If there is a conflict, you MUST fix it, compile to make sure
            // your program compiles and runs locally, then push to the git repository on GitHub.
